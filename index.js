8a50eba207bed11ca9ea176726a3491336ab3feb
rizkyalvie <rizkyalviee@gmail.com>
2022-07-25 03:45:24 +0700

    First Commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const app = Vue.createApp({
+    data(){
+        return{
+            firstName: 'Rizky',
+            lastName: 'Alvie',
+            email: 'alvienuryahya@gmail.com',
+            gender: 'male',
+            picture: './me.jpg'
+        }
+    },
+    methods: {
+        async getUser() {
+            const res = await fetch('https://randomuser.me/api')
+            const { results } = await res.json()
+
+            console.log(results)
+
+            this.firstName = results[0].name.first
+            this.lastName = results[0].name.last
+            this.email = results[0].email
+            this.gender = results[0].gender
+            this.picture = results[0].picture.large
+        }
+    }
+})
+
+app.mount('#app')
